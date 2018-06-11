c1b413955fd5f2afd929027b0ec2ea5163fac427
thinktech2go <thinktech2go@gmail.com>
2018-06-11 02:07:29 -0400

    full editor

---
+++ View/Items.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace SaveEditor.View
+{
+    /// <summary>
+    /// Interaction logic for Items.xaml
+    /// </summary>
+    public partial class Items
+    {
+        public Items()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
